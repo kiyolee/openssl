44d9b91af048e977411050deca87ae752ecde21a
Todd Short <tshort@akamai.com>
2019-08-15 12:37:03 -0400

    Use proper secrets for handshake

--- ssl/ssl_quic.c
+++ ssl/ssl_quic.c
@@ -199,8 +199,8 @@ int quic_set_encryption_secrets(SSL_CONNECTION *sc, OSSL_ENCRYPTION_LEVEL level)
         s2c_secret = sc->early_secret;
         break;
     case ssl_encryption_handshake:
-        c2s_secret = sc->client_finished_secret;
-        s2c_secret = sc->server_finished_secret;
+        c2s_secret = sc->client_hand_traffic_secret;
+        s2c_secret = sc->server_hand_traffic_secret;
         break;
     case ssl_encryption_application:
         c2s_secret = sc->client_app_traffic_secret;
