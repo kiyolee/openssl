b0b7b95ac93649a83262eebed65cfe7fa2bb9f62
Tatsuhiro Tsujikawa <tatsuhiro.t@gmail.com>
2020-12-12 17:27:46 +0900

    QUIC v1 quic_transport_parameters

--- ssl/ssl_local.h
+++ ssl/ssl_local.h
@@ -697,7 +697,8 @@ typedef enum tlsext_index_en {
     TLSEXT_IDX_compress_certificate,
     TLSEXT_IDX_early_data,
     TLSEXT_IDX_certificate_authorities,
-    TLSEXT_IDX_quic_transport_params,
+    TLSEXT_IDX_quic_transport_params_draft,
+    TLSEXT_IDX_quic_transport_params_v1,
     TLSEXT_IDX_padding,
     TLSEXT_IDX_psk,
     /* Dummy index - must always be the last entry */
@@ -1711,8 +1712,10 @@ struct ssl_connection_st {
 #ifndef OPENSSL_NO_QUIC_BORING
         uint8_t *quic_transport_params;
         size_t quic_transport_params_len;
-        uint8_t *peer_quic_transport_params;
-        size_t peer_quic_transport_params_len;
+        uint8_t *peer_quic_transport_params_draft;
+        size_t peer_quic_transport_params_draft_len;
+        uint8_t *peer_quic_transport_params_v1;
+        size_t peer_quic_transport_params_v1_len;
 #endif
     } ext;
 
