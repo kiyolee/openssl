eb893128c1ad3196d8369e22152efb8d8891affa
Kelvin Lee <kiyolee@gmail.com>
2023-02-22 17:48:23 +1100

    Update following upstream

--- ms/x64/static/configdata.pm
+++ ms/x64/static/configdata.pm
@@ -302,7 +302,7 @@ our %target = (
     "LDFLAGS" => "/nologo /debug",
     "MT" => "mt",
     "MTFLAGS" => "-nologo",
-    "RANLIB" => "CODE(0x26afcf0)",
+    "RANLIB" => "CODE(0xe008a0)",
     "RC" => "rc",
     "_conf_fname_int" => [
         ".\\Configurations\\00-base-templates.conf",
@@ -1435,6 +1435,9 @@ our %unified_info = (
                 "noinst" => "1"
             },
             "test\\namemap_internal_test" => {
+                "noinst" => "1"
+            },
+            "test\\nodefltctxtest" => {
                 "noinst" => "1"
             },
             "test\\ocspapitest" => {
@@ -8103,6 +8106,10 @@ our %unified_info = (
             "test\\libtestutil.a"
         ],
         "test\\namemap_internal_test" => [
+            "libcrypto.a",
+            "test\\libtestutil.a"
+        ],
+        "test\\nodefltctxtest" => [
             "libcrypto.a",
             "test\\libtestutil.a"
         ],
@@ -19951,6 +19958,12 @@ our %unified_info = (
             ".\\include",
             ".\\apps\\include"
         ],
+        "test\\nodefltctxtest" => [
+            "include",
+            "apps\\include",
+            ".\\include",
+            ".\\apps\\include"
+        ],
         "test\\ocspapitest" => [
             "include",
             "apps\\include",
@@ -21588,6 +21601,7 @@ our %unified_info = (
         "test\\memleaktest",
         "test\\modes_internal_test",
         "test\\namemap_internal_test",
+        "test\\nodefltctxtest",
         "test\\ocspapitest",
         "test\\ossl_store_test",
         "test\\packettest",
@@ -27506,6 +27520,12 @@ our %unified_info = (
         ],
         "test\\namemap_internal_test-bin-namemap_internal_test.o" => [
             ".\\test\\namemap_internal_test.c"
+        ],
+        "test\\nodefltctxtest" => [
+            "test\\nodefltctxtest-bin-nodefltctxtest.o"
+        ],
+        "test\\nodefltctxtest-bin-nodefltctxtest.o" => [
+            ".\\test\\nodefltctxtest.c"
         ],
         "test\\ocspapitest" => [
             "test\\ocspapitest-bin-ocspapitest.o"
