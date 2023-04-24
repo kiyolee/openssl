7be3666aec0f9fa6d517ef4305ba13348da8e448
Kelvin Lee <kiyolee@gmail.com>
2023-04-25 00:13:05 +1000

    Differentiate BoringSSL QUIC code

--- ssl/statem/statem_local.h
+++ ssl/statem/statem_local.h
@@ -113,7 +113,7 @@ __owur int tls_get_message_header(SSL_CONNECTION *s, int *mt);
 __owur int tls_get_message_body(SSL_CONNECTION *s, size_t *len);
 __owur int dtls_get_message(SSL_CONNECTION *s, int *mt);
 __owur int dtls_get_message_body(SSL_CONNECTION *s, size_t *len);
-__owur int quic_get_message(SSL_CONNECTION *s, int *mt, size_t *len);
+__owur int quic_boring_get_message(SSL_CONNECTION *s, int *mt, size_t *len);
 
 /* Message construction and processing functions */
 __owur int tls_process_initial_server_flight(SSL_CONNECTION *s);
@@ -341,10 +341,10 @@ int tls_parse_ctos_post_handshake_auth(SSL_CONNECTION *, PACKET *pkt,
     unsigned int context,
     X509 *x, size_t chainidx);
 #ifndef OPENSSL_NO_QUIC_BORING
-int tls_parse_ctos_quic_transport_params_draft(SSL_CONNECTION *s, PACKET *pkt,
+int tls_parse_ctos_quic_boring_transport_params_draft(SSL_CONNECTION *s, PACKET *pkt,
     unsigned int context, X509 *x,
     size_t chainidx);
-int tls_parse_ctos_quic_transport_params_v1(SSL_CONNECTION *s, PACKET *pkt,
+int tls_parse_ctos_quic_boring_transport_params_v1(SSL_CONNECTION *s, PACKET *pkt,
     unsigned int context, X509 *x,
     size_t chainidx);
 #endif
@@ -415,11 +415,11 @@ EXT_RETURN tls_construct_stoc_psk(SSL_CONNECTION *s, WPACKET *pkt,
     unsigned int context,
     X509 *x, size_t chainidx);
 #ifndef OPENSSL_NO_QUIC_BORING
-EXT_RETURN tls_construct_stoc_quic_transport_params_draft(SSL_CONNECTION *s, WPACKET *pkt,
+EXT_RETURN tls_construct_stoc_quic_boring_transport_params_draft(SSL_CONNECTION *s, WPACKET *pkt,
     unsigned int context,
     X509 *x,
     size_t chainidx);
-EXT_RETURN tls_construct_stoc_quic_transport_params_v1(SSL_CONNECTION *s, WPACKET *pkt,
+EXT_RETURN tls_construct_stoc_quic_boring_transport_params_v1(SSL_CONNECTION *s, WPACKET *pkt,
     unsigned int context,
     X509 *x,
     size_t chainidx);
@@ -507,11 +507,11 @@ EXT_RETURN tls_construct_ctos_post_handshake_auth(SSL_CONNECTION *s, WPACKET *pk
     unsigned int context,
     X509 *x, size_t chainidx);
 #ifndef OPENSSL_NO_QUIC_BORING
-EXT_RETURN tls_construct_ctos_quic_transport_params_draft(SSL_CONNECTION *s, WPACKET *pkt,
+EXT_RETURN tls_construct_ctos_quic_boring_transport_params_draft(SSL_CONNECTION *s, WPACKET *pkt,
     unsigned int context,
     X509 *x,
     size_t chainidx);
-EXT_RETURN tls_construct_ctos_quic_transport_params_v1(SSL_CONNECTION *s, WPACKET *pkt,
+EXT_RETURN tls_construct_ctos_quic_boring_transport_params_v1(SSL_CONNECTION *s, WPACKET *pkt,
     unsigned int context,
     X509 *x,
     size_t chainidx);
@@ -568,10 +568,10 @@ int tls_parse_stoc_cookie(SSL_CONNECTION *s, PACKET *pkt, unsigned int context,
 int tls_parse_stoc_psk(SSL_CONNECTION *s, PACKET *pkt, unsigned int context,
     X509 *x, size_t chainidx);
 #ifndef OPENSSL_NO_QUIC_BORING
-int tls_parse_stoc_quic_transport_params_draft(SSL_CONNECTION *s, PACKET *pkt,
+int tls_parse_stoc_quic_boring_transport_params_draft(SSL_CONNECTION *s, PACKET *pkt,
     unsigned int context, X509 *x,
     size_t chainidx);
-int tls_parse_stoc_quic_transport_params_v1(SSL_CONNECTION *s, PACKET *pkt,
+int tls_parse_stoc_quic_boring_transport_params_v1(SSL_CONNECTION *s, PACKET *pkt,
     unsigned int context, X509 *x,
     size_t chainidx);
 #endif
