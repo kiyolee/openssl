431bae712dc14a276c9542c7ae6dd154f2af566f
Kelvin Lee <kiyolee@gmail.com>
2025-11-20 20:42:33 +1100

    Update following upstream

--- ms/x86/static/configdata.pm
+++ ms/x86/static/configdata.pm
@@ -9956,6 +9956,10 @@ our %unified_info = (
             "libssl.a",
             "test\\libtestutil.a"
         ],
+        "test\\x509-memfail" => [
+            "libcrypto.a",
+            "test\\libtestutil.a"
+        ],
         "test\\x509_acert_test" => [
             "libcrypto",
             "test\\libtestutil.a"
@@ -24253,6 +24257,12 @@ our %unified_info = (
             ".\\apps\\include"
         ],
         "test\\wpackettest" => [
+            "include",
+            "apps\\include",
+            ".\\include",
+            ".\\apps\\include"
+        ],
+        "test\\x509-memfail" => [
             "include",
             "apps\\include",
             ".\\include",
